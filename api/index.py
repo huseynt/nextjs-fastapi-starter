cc411009c9591a0be20687ef1f87f7fcf63ca51a
Huseyn Tapdigli <74926067+huseynt@users.noreply.github.com>
2024-12-01 21:37:01 +0400

    _

--- api/index.py
+++ api/index.py
@@ -3,12 +3,12 @@ app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 
 
 
-@app.get("/")
+@app.get("/api/py/helloFastApi")
 def read_root():
     return {"message": "hello !"}
 
 
-@app.get("/api/hello")
+@app.get("/api/py/hello")
 def hello_fast_api():
     return {"message": "Hello from FastAPI"}
 
@@ -16,6 +16,6 @@ def hello_fast_api():
 def hello_fast_api_name(name: str):
     return {"message": f"Hello {name} from FastAPI"}
 
-@app.post("/api/predict")
+@app.post("/api/py/predict")
 def hello_fast_api_post(body: dict, headers: dict):
     return {"message": "Hello from FastAPI POST", "body": body, "headers": headers}
